777b5cf3d8fcad4e536694fe26833c07413f19e0
MjKamma <Mercyigwe1995@gmail.com>
2023-07-31 00:25:26 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,47 +1,39 @@
 #!/usr/bin/python3
-'''module to validate UTF8 data'''
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    '''check for validity of data in UTF8'''
+    """Determines if a given data set
+    represents a valid utf-8 encoding
+    """
+    number_bytes = 0
 
-    # Helper function to check if a given byte is a valid UTF-8 start byte
-    def is_start_byte(byte):
-        '''Helper function to check if a given
-        byte is a valid UTF-8 start byte'''
-        return ((byte & 0b10000000) == 0b00000000
-                or (byte & 0b11100000) == 0b11000000
-                or (byte & 0b11110000) == 0b11100000
-                or (byte & 0b11111000) == 0b11110000)
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    # Helper function to get the number of
-    # continuation bytes based on the start byte
-    def get_num_cont_bytes(start_byte):
-        '''Helper function to get the number
-        of continuation bytes based on the start byte'''
-        if (start_byte & 0b11100000) == 0b11000000:
-            return 1
-        elif (start_byte & 0b11110000) == 0b11100000:
-            return 2
-        elif (start_byte & 0b11111000) == 0b11110000:
-            return 3
-        return 0
+    for i in data:
 
-    index = 0
-    while index < len(data):
-        start_byte = data[index]
-        num_cont_bytes = get_num_cont_bytes(start_byte)
+        mask_byte = 1 << 7
 
-        # Check if the start byte is a valid UTF-8 start byte
-        if (not is_start_byte(start_byte)
-                or num_cont_bytes + index + 1 > len(data)):
-            return False
+        if number_bytes == 0:
 
-        # Check continuation bytes
-        for i in range(index + 1, index + num_cont_bytes + 1):
-            if (data[i] & 0b11000000) != 0b10000000:
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
 
-        index += num_cont_bytes + 1
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
 
-    return True
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
