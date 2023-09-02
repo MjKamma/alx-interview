c089b175b3a5e24e5b584062d80a256393b975ae
MjKamma <Mercyigwe1995@gmail.com>
2023-09-02 22:55:58 +0000

    tasks

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,7 @@ Python function that implements the logic to calculate the
 perimeter of the island described in the given grid
 """
 
+
 def island_perimeter(grid):
     """calculate the perimeter of the island"""
     if not grid:
